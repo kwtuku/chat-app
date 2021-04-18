fa95217cd6a8c4a38250df16d962c189fac79719
kwtuku <ttyttyttitti@gmail.com>
2021-04-18 15:43:42 +0900

    userモデルとヘッダーを作成した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root  'hello#index'
+  devise_for :users
+  root  'rooms#show'
   get 'hello/index', to: 'hello#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'hello/create', to: 'hello#create'
