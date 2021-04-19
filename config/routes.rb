ed514d1b7440be708ba543ae7c758bd001de878e
kwtuku <ttyttyttitti@gmail.com>
2021-04-19 23:20:57 +0900

    メッセージモデルと初期データを追加した

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'hello/index', to: 'hello#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'hello/create', to: 'hello#create'
+  resources :messages, only: :create
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_055945) do
+ActiveRecord::Schema.define(version: 2021_04_19_135137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2021_04_18_055945) do
   create_table "hellos", force: :cascade do |t|
     t.text "hello"
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
