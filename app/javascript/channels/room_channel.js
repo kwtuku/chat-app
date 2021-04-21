a894b3f510b889a31d9388297822f93f5784563c
kwtuku <ttyttyttitti@gmail.com>
2021-04-21 22:29:17 +0900

    メッセージを開いたとき、メッセージを投稿後にページの一番下に移動するようにした

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,22 +1,31 @@
 import consumer from "./consumer"
 
 document.addEventListener('turbolinks:load', () => {
-
   window.messageContainer = document.getElementById('message-container')
 
   if (messageContainer === null) {
-      return
+    return
   }
 
   consumer.subscriptions.create("RoomChannel", {
-      connected() {
-      },
+    connected() {
+    },
 
-      disconnected() {
-      },
+    disconnected() {
+    },
 
-      received(data) {
-          messageContainer.insertAdjacentHTML('beforeend', data['message'])
-      }
+    received(data) {
+      messageContainer.insertAdjacentHTML('beforeend', data['message'])
+    }
   })
+
+  const documentElement = document.documentElement
+
+  window.messageContent = document.getElementById('message_content')
+
+  window.scrollToBottom = () => {
+    window.scroll(0, documentElement.scrollHeight)
+  }
+
+  scrollToBottom()
 })
