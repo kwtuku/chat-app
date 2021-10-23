340eabb376d508a41c330a5ad04c851f692d03b2
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-10-24 00:23:34 +0900

    chore: hellosテーブルに関するファイルを削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
   devise_for :users
   root  'rooms#show'
-  get 'hello/index', to: 'hello#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post 'hello/create', to: 'hello#create'
   resources :messages, only: :create
   get '/show_additionally', to: 'rooms#show_additionally'
 end
