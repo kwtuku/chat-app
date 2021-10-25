53cb45d44df34003fc2845a963a0024ea5f5427a
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-10-25 21:13:48 +0900

    test: システムスペックが動くようにcapybaraを設定

--- Gemfile
+++ Gemfile
@@ -47,12 +47,8 @@ group :development do
 end
 
 group :test do
-  # Adds support for Capybara system testing and selenium driver
-  gem 'capybara', '>= 2.15'
+  gem 'capybara'
   gem 'selenium-webdriver'
-  # Easy installation and use of web drivers to run system tests with browsers
-  gem 'webdrivers'
-
   gem 'shoulda-matchers'
 end
 
---
+++ spec/system/users_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :system do
+  let(:alice) { create :user }
+
+  it 'signs in' do
+    visit new_user_session_path
+    fill_in 'user[email]', with: alice.email
+    fill_in 'user[password]', with: alice.password
+    click_button 'ログイン'
+    expect(page).to have_content 'ログインしました。'
+  end
+
+  it 'works with js', js: true do
+    visit new_user_session_path
+    fill_in 'user[email]', with: alice.email
+    fill_in 'user[password]', with: alice.password
+    click_button 'ログイン'
+    expect(page).to have_content 'ログインしました。'
+    find('.close').click
+    expect(page).not_to have_content 'ログインしました。'
+  end
+end
