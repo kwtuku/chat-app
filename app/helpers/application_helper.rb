3783a0ffad25329dbe679971b49cac20af68fda5
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-10-26 17:56:35 +0900

    refactor: メッセージ画面のスタイルを変更した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def message_created_at(created_at)
+    if created_at > Time.now.yesterday
+      l created_at, format: :today
+    elsif created_at > Time.now.last_year
+      l created_at, format: :this_year
+    else
+      l created_at, format: :before_this_year
+    end
+  end
 end
