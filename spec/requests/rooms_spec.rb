961b88bf63a42d49bdd683f31b6c7f8f3195ad18
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-10-27 20:23:18 +0900

    test: 6fb35ecで変更し忘れたパスを変更、メッセージではなくルームが表示されることのテストを追加

--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe 'Rooms', type: :request do
   describe 'GET /rooms' do
     context 'when not signed in' do
       it 'returns a 302 response' do
-        get root_path
+        get rooms_path
         expect(response.status).to eq 302
       end
 
       it 'redirects to new_user_session_path' do
-        get root_path
+        get rooms_path
         expect(response).to redirect_to new_user_session_path
       end
     end
 
     context 'when signed in' do
       let(:alice) { create :user }
-      let!(:messages) { create_list(:message, 5) }
+      let!(:room) { alice.rooms.create! }
 
       before { sign_in alice }
 
@@ -25,9 +25,9 @@ RSpec.describe 'Rooms', type: :request do
         expect(response.status).to eq 200
       end
 
-      it 'shows messages' do
+      it 'shows rooms' do
         get root_path
-        expect(response.body).to include messages.sample.content
+        expect(response.body).to include "Room#{room.id}（#{alice.id}）"
       end
     end
   end
