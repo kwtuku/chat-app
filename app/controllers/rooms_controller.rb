28b9742b5faee6a27724098b1adbd07ba463b9d1
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-11-13 21:11:36 +0900

    refactor: N+1クエリを解消

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,13 +3,13 @@ class RoomsController < ApplicationController
 
   def index
     authorize User
-    @rooms = current_user.rooms.all
+    @rooms = current_user.rooms.all.eager_load(:entries, :users)
   end
 
   def show
     @room = Room.find(params[:id])
     authorize @room
-    @rooms = current_user.rooms.all
+    @rooms = current_user.rooms.all.eager_load(:entries, :users)
     @messages = @room.messages.includes(:user).order(:id).last(50)
     @message = current_user.messages.build
   end
