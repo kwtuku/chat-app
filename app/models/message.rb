cd59f909a687c6f04107fb2de65fe8fc44e02624
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-12-29 20:09:32 +0900

    refactor: render_with_signed_in_userメソッドを削除
    viewでcurrent_userを使うために必要だが、action cableでレンダーされるときはcurrent_userが機能しないため

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,10 +6,10 @@ class Message < ApplicationRecord
   validates :content, presence: true, length: { maximum: 500 }
 
   def partial_for_current_user
-    ApplicationController.render_with_signed_in_user(user, partial: 'messages/message', locals: { message: self })
+    ApplicationController.renderer.render(partial: 'messages/current_user_message', locals: { message: self })
   end
 
   def partial_for_other_user
-    ApplicationController.render_with_signed_in_user(user, partial: 'messages/other_user_message', locals: { message: self })
+    ApplicationController.renderer.render(partial: 'messages/other_user_message', locals: { message: self })
   end
 end
