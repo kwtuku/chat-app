627282f7eeb2e686e12c3b25d36f4f66fad98676
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-12-30 10:27:56 +0900

    refactor: action cableの処理をjobに移動

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,12 +4,4 @@ class Message < ApplicationRecord
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 500 }
-
-  def partial_for_current_user
-    ApplicationController.renderer.render(partial: 'messages/current_user_message', locals: { message: self })
-  end
-
-  def partial_for_other_user
-    ApplicationController.renderer.render(partial: 'messages/other_user_message', locals: { message: self })
-  end
 end
